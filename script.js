a7362c9720bdb6ac6c0b3887e5e3140070652153
francinepaixao-spec <francine.paixao@escola.pr.gov.br>
2026-09-02 09:30:28 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
